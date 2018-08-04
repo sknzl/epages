74d17b6b9717cf979903f745610048ede43e4de5
Swen Kunzel <swenkuenzel@gmail.com>
2018-08-04 16:22:28 +0100

    creation, deletion and update of shops

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,7 +1,36 @@
 class ShopsController < ApplicationController
-  before_action :set_shop, only: [:show]
+  before_action :set_shop, only: [:show, :destroy, :edit, :update]
   def index
     @shops = Shop.all
+  end
+
+  def new
+    @shop = Shop.new
+  end
+
+  def create
+    @shop = Shop.new(shop_params)
+    if @shop.save
+      redirect_to shops_path
+    else
+      render "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @shop.update_attributes(shop_params)
+      redirect_to shops_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @shop.destroy
+    redirect_to shops_path
   end
 
   def show
@@ -17,6 +46,10 @@ class ShopsController < ApplicationController
   end
 
   private
+
+  def shop_params
+    params.require(:shop).permit(:name, :host, :token)
+  end
 
   def set_shop
     @shop = Shop.find(params[:id])
