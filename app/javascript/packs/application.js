e7bb01c1459e8a2101995681e5c33754a983c954
Swen Kunzel <swenkuenzel@gmail.com>
2018-08-04 17:29:38 +0100

    added html css and jquery for spinner

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,23 @@
 import "bootstrap";
+
+$( document ).ready(function() {
+
+  // hide spinner
+  console.log("hide spinner")
+  $(".spinner").hide();
+
+
+  // // show spinner on AJAX start
+  // $(document).ajaxStart(function(){
+  //   console.log("show spinner")
+  //   $(".spinner").show();
+  // });
+
+  // // hide spinner on AJAX stop
+  // $(document).ajaxStop(function(){
+  //   console.log("hide spinner")
+  //   $(".spinner").hide();
+  // });
+
+});
+
